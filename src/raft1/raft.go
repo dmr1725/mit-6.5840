4194eed865c20ef20e257908da41696448865e03
Diego <diegozmendez@gmail.com>
2026-04-06 23:04:18 -0400

    Aquire lock before signaling to fix tests

--- src/raft1/raft.go
+++ src/raft1/raft.go
@@ -607,7 +607,9 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 func (rf *Raft) Kill() {
 	atomic.StoreInt32(&rf.dead, 1)
 	// Your code here, if desired.
+	rf.mu.Lock()
 	rf.applyCond.Signal() // signal the condition variable so the applier can exit
+	rf.mu.Unlock()
 }
 
 func (rf *Raft) killed() bool {
